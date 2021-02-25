4f204fc022bf2b56d00a22b95b77c80e400955b8
wonkatin <j.drawers@gmail.com>
2021-02-25 17:22:05 -0600

    set up cookies

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const axios = require('axios')
 const db = require('./models')
 const ejsLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
+const cookieParser = require('cookie-parser')
+const models = require('./models')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -20,11 +22,18 @@ app.use(methodOverride('_method'))
 // app.use(require('express-ejs-layouts'))
 app.use(require('morgan')('tiny'))
 // app.use(require('morgan')('dev'))
-
+app.use(cookieParser())
+app.use(async (req, res, next) => {
+  const user = await models.user.findByPk(req.cookies.userId)
+  res.locals.user = user
+  next()
+})
 app.use('/users', require('./controllers/usersControllers'))
 
 // routes
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  // const user = await models.user.findByPk(req.cookies.userId)
+  console.log(res.user)
   res.render('index')
 })
 
