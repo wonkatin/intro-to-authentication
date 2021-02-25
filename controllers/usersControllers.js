c7a62b4692c170340a55e8910138ddede1a9bb25
wonkatin <j.drawers@gmail.com>
2021-02-25 16:33:04 -0600

    updating class work

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -15,4 +15,20 @@ router.post('/', async (req, res)=> {
     res.redirect('/')
 })
 
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', async (req, res) => {
+    const user = await db.user.findOne({
+        where: { email: req.body.email}
+    })
+    if (user.password === req.body.password) {
+        res.cookie('userId', user.id)
+        res.redirect('/')
+    } else {
+        res.render('users/login')
+    }
+})
+
 module.exports = router
